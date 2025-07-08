32a3cc0c0d1db7fd66da561671ebaafe8ddee142
Aaditya Adhikari <aadityaadhikari@Aadityas-MacBook-Air.local>
2025-07-08 18:32:32 +0545

    cookiesettings

--- controllers/Usercontroller.js
+++ controllers/Usercontroller.js
@@ -13,31 +13,41 @@ exports.getSingleUser = async (req, res) => {
     let user = await User.findOne({ "uniqueid": req.query.uniqueid, "password": req.query.password })
 
     if (!user) {
-        res.json({ message: "doesnt exist" }).status(500);
+        res.status(500).json({ message: "doesnt exist" });
     }
     else {
 
         jwt.sign({ uniqueid: user.uniqueid, id: user._id }, secretkey, {}, (err, token) => {
             if (err) throw err;
-            res.cookie('token', token).json('okay').status(200);
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'None'
+            }).status(200).json('okay');
         })
     }
 }
 
 exports.getProfile = async (req, res) => {
 
+
+
     const { token } = req.cookies;
-    
+
+    if (!token) return res.status(500).json("no token")
+
+
+
     jwt.verify(token, secretkey, {}, (err, info) => {
         if (err) {
             //first ma ta token nai hudaina so tyo error ni aauxa
             //unverified token vayo vanae pani error nai ta aauxa
             //console.log("error in verifying or token is not verified")
-            res.status(500).json("bad")
+            res.status(500).json("bad");
             //note status first hunu parxa json vanda paila
         }
         else {
-            res.status(200).json(info)
+            res.status(200).json(info);
         }
 
     })
@@ -45,7 +55,11 @@ exports.getProfile = async (req, res) => {
 
 exports.userLogout = async (req, res) => {
 
-    res.cookie('token','').json('okay').status(200);
+    res.cookie('token', '',{
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None'
+        }).status(200).json('okay');
     //tyo jwt le na banako token or empty pathai dini aani ta vai halyo
     //cookie reset gare ko
 }
